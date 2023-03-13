c53b953889bf6cd5ba25ddd308b55ef7f4619c25
ChristianAlonso16 <105463895+ChristianAlonso16@users.noreply.github.com>
2023-03-12 22:45:10 -0600

    finish get buildings

--- src/admin/components/LaboratoriesComponent.js
+++ src/admin/components/LaboratoriesComponent.js
@@ -1,107 +1,46 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom';
+import { getBuilding } from '../services/LaboratoriesService';
+import Loading from '../../main/components/Loading'
 const LaboratoriesComponent = () => {
+    const [building, setBuilding] = useState([]);
+
+    useEffect(() => {
+        const findLab = async () => {
+            const response = await getBuilding();
+            setBuilding(response.data.data);
+        }
+        findLab()
+    }, [])
+    const navigate = useNavigate();
+
+    const irAulas = () => {
+        navigate("/admin/aulas")
+    }
+    if (!building.length) return <Loading />
+
     return (
-        <div className="container-md py-5 mt-5  " style={{paddingLeft: "80px"}}>
+        <div className="container-md py-5 mt-5  " style={{ paddingLeft: "80px" }}>
             <div className=" row g-3 d-flex">
-                <div className="col-md-6 d-flex justify-content-end ">
-                    <div className="col-lg-7">
-                        <div className="card rounded-5 bg-secondary" >
-                            <div className="row g-0 ">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
-                                </div>
-                                <div className="col  d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-white">
-                                        Docencia 3
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 d-flex justify-content-end">
-                    <div className="col-lg-7">
-                        <div className="card rounded-5 bg-secondary" >
-                            <div className="row g-0 ">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
-                                </div>
-                                <div className="col  d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-white">
-                                        Docencia 3
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 d-flex justify-content-end">
-                    <div className="col-lg-7">
-                        <div className="card rounded-5 bg-secondary" >
-                            <div className="row g-0 ">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
-                                </div>
-                                <div className="col  d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-white">
-                                        Docencia 3
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 d-flex justify-content-end">
-                    <div className="col-lg-7">
-                        <div className="card rounded-5 bg-secondary" >
-                            <div className="row g-0 ">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
-                                </div>
-                                <div className="col  d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-white">
-                                        Docencia 3
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 d-flex justify-content-end">
-                    <div className="col-lg-7">
-                        <div className="card rounded-5 bg-secondary" >
-                            <div className="row g-0 ">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
-                                </div>
-                                <div className="col  d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-white">
-                                        Docencia 3
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-6 d-flex justify-content-end ">
-                    <div className="col-lg-7">
-                        <div className="card rounded-5 bg-secondary" >
-                            <div className="row g-0 ">
-                                <div className="col-md-6 col-lg-5 d-none d-md-block">
-                                    <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
-                                </div>
-                                <div className="col  d-flex align-items-center">
-                                    <div className="card-body p-4 p-lg-5 text-white">
-                                        Docencia 3
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                {building.map((build) => (
 
-
+                    <div className="col-md-6 d-flex justify-content-end " key={build.id} >
+                        <div className="col-lg-7">
+                            <div className="card rounded-5 bg-secondary" onClick={irAulas}>
+                                <div className="row g-0 ">
+                                    <div className="col-md-6 col-lg-5 d-none d-md-block">
+                                        <img src="./docencia.png" className="img-fluid rounded-5" alt="" />
+                                    </div>
+                                    <div className="col  d-flex align-items-center">
+                                        <div className="card-body p-4 p-lg-5 text-white">
+                                            {build.name}
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            ))}
             </div>
 
         </div>
